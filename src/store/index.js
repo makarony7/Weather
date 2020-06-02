d3b8587e0acd77417c8180ea122785932ebc01c7
Janek <makarony7@wp.pl>
2020-06-02 16:03:24 +0200

    getters and dispatch on red

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import service from '../service/service.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    isTabFlagCurrent: null,
+    locationSearch: null,
+    weatherCache: null,
   },
   mutations: {
+    setTabFlag: (state, payload) => state.isTabFlagCurrent = payload,
+    setlocationSearch:(state,payload) => state.locationSearch = payload,
+    setWeatherCache: (state, payload) => state.weatherCache = payload
   },
   actions: {
+    async searchLocation({ getters, commit, dispatch }, payload) {
+      let wF = await service.getWeather(payload + '&days=7');
+      commit('setWeatherCache', wF)
+    }
   },
-  modules: {
+  getters:{
+    getTabFlag: (state) => state.isTabFlagCurrent,
+    getlocationSearch: (state) => state.locationSearch,
+    getWeatherCache: (state) => state.weatherCache,
   }
 })
